ed886e5dda1e86866c4c6c62be30fe115f788af4
song-gld <381471766@qq.com>
2020-02-22 16:56:24 +0800

    edit favorite's view file

--- runtime/temp/6fcc06010db00d54912dd4f39c4b21d3.php
+++ runtime/temp/6fcc06010db00d54912dd4f39c4b21d3.php
@@ -1,4 +1,4 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:78:"D:\xampp\htdocs\fastadmin\public/../application/index\view\favorite\index.html";i:1582276280;}*/ ?>
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:78:"D:\xampp\htdocs\fastadmin\public/../application/index\view\favorite\index.html";i:1582361680;}*/ ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,6 +10,9 @@
 <style>
 	.ico{
 		height: 30px;
+	}
+	.col-md-2{
+		padding: 20px;
 	}
 	a{
 		font-size: 20px;
@@ -26,21 +29,36 @@
 			    <div class="navbar-header">
 			        <a class="navbar-brand" href="#">网络收藏夹</a>
 			    </div>
-			    <div>
-			        <form class="navbar-form navbar-left" role="search">
-			            <div class="form-group">
-			                <input type="text" class="form-control" placeholder="Search">
-			            </div>
-			            <button type="submit" class="btn btn-default">提交按钮</button>
-			        </form>
-			        <button type="button" class="btn btn-default navbar-btn">
-			            导航栏按钮
-			        </button>
-			    </div>
+				    <div>
+				    	 <!-- 头部搜索条 -->
+				        <form class="navbar-form navbar-left" role="search">
+				            <div class="form-group">
+				                <input type="text" class="form-control" placeholder="Search">
+				            </div>
+				            <button type="submit" class="btn btn-default">查找</button>
+				        </form>
+				        <!--//添加新网址-->
+				        <div class="navbar-header">
+			        		<a class="navbar-brand" href="#">添加新网址</a>
+			    		</div>
+				        <form class="navbar-form navbar-left" role="search">
+				            <div class="form-group">
+				                <input type="text" class="form-control" placeholder="网站名称">
+				            </div>
+				            <div class="form-group">
+				                <input type="text" class="form-control" placeholder="网址">
+				            </div>
+				            <div class="form-group input-small">
+				                <input type="text" class="form-control col-sd" placeholder="分类">
+				            </div>
+				            <button type="submit" class="btn btn-default">新增</button>
+				        </form>			        
+				    </div>
 			    </div>
 			</nav>
 		</div>
 	</div>
+	<!-- //网址显示区 -->
 	<div class="row">
 		<?php if(is_array($data) || $data instanceof \think\Collection || $data instanceof \think\Paginator): $i = 0; $__LIST__ = $data;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i;?>
 			<div class="col-md-2">
